26b74217ff4da846fc28ac667375c682c849699e
Caleb Pendergraft <cackmed@gmail.com>
2019-12-04 15:01:00 -0800

    Schema test

---
+++ _tests_/schema.test.js
@@ -0,0 +1,29 @@
+const Schema = require('../lib/Schema');
+
+describe('Schema test module', () => {
+  it('validates a working schema', () => {
+    const schema = new Schema({
+      name: {
+        type: String,
+        required: true
+      },
+      age: {
+        type: Number
+      },
+      favoriteWord: {
+        type: String
+      }
+    });
+
+    const person = {
+      name: 'Joe',
+      age: 23,
+      favoriteWord: 'false'
+    };
+    expect(schema.validate(person)).toEqual({
+      name: 'Joe',
+      age: 23,
+      favoriteWord: 'false'
+    });
+  });
+});
